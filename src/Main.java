bc22f1931e42ec5c424490d751da93f9a6a2ce63
Sam Ly <sam@samly.me>
2024-09-19 12:52:13 -0700

    refactor solver class

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,21 @@
-import java.util.Arrays;
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
         int vars = 3;
-        float[][] coeff =
-                {
-                        {3f, 4f, 1f},
-                        {2f, 3f, 0f},
-                        {4f, 3f, -1f},
-                };
         float[] constants = {1, 0, -2};
+        float[][] coeff = {
+                {3f, 4f, 1f},
+                {2f, 3f, 0f},
+                {4f, 3f, -1f},
+        };
         NaiveGaussianEliminationSinglePrecision test = new NaiveGaussianEliminationSinglePrecision(vars, coeff, constants);
 
-        System.out.println(Arrays.toString(test.solve()));
+        final long startTime = System.nanoTime();
+        test.solve();
+        final long duration = System.nanoTime() - startTime;
+
+        System.out.println(test.getSolution());
+        System.out.println(duration);
     }
 }
--- src/NaiveGaussianEliminationSinglePrecision.java
+++ src/NaiveGaussianEliminationSinglePrecision.java
@@ -3,15 +3,18 @@ import java.util.Arrays;
 public final class NaiveGaussianEliminationSinglePrecision {
     private final float[][] coefficients;
     private final float[] constants;
+    private final float[] solutions;
     private final int variables;
 
     public NaiveGaussianEliminationSinglePrecision(int variables, float[][] coefficients, float[] constants) {
         this.variables = variables;
         this.coefficients = coefficients;
         this.constants = constants;
+
+        this.solutions = new float[variables];
     }
 
-    public float[] solve() {
+    public void solve() {
         // Forward elimination
 
         // Pivot goes from 0 to variables - 1
@@ -19,9 +22,7 @@ public final class NaiveGaussianEliminationSinglePrecision {
             // Go through each row below the pivot
             for (int row = pivot + 1; row < variables; row++) {
                 // Each row gets scaled so that a coefficient can be eliminated
-                //
                 float scale = coefficients[row][pivot] / coefficients[pivot][pivot];
-                System.out.println("scale: " + scale);
 
                 // Eliminate
                 for (int col = pivot; col < variables; col++) {
@@ -30,22 +31,25 @@ public final class NaiveGaussianEliminationSinglePrecision {
 
                 // do same for constants
                 constants[row] -= scale * constants[pivot];
-                System.out.println(Arrays.deepToString(coefficients));
             }
         }
 
 
-        float[] sol = new float[variables];
-        sol[variables-1] = constants[variables-1] / coefficients[variables-1][variables-1];
+        // Back substitution
+        solutions[variables-1] = constants[variables-1] / coefficients[variables-1][variables-1];
 
         for (int i = variables-2; i >= 0; i--) {
             float sum = constants[i];
             for (int j = i + 1; j < variables; j++) {
-                sum = sum -coefficients[i][j] * sol[j];
+                sum = sum -coefficients[i][j] * solutions[j];
             }
-            sol[i] = sum / coefficients[i][i];
+            solutions[i] = sum / coefficients[i][i];
         }
+    }
 
-        return sol;
+    // This design is because I will need to implement the same alg with a different
+    // primitive for double precision.
+    public String getSolution() {
+        return Arrays.toString(solutions);
     }
 }
