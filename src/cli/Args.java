c192d75c72749a4a491bbc69c3ce2e26afa7d8ba
Sam Ly <sam@samly.me>
2024-09-24 15:37:17 -0700

    implement args

--- src/cli/Args.java
+++ src/cli/Args.java
@@ -3,9 +3,36 @@ package cli;
 import com.beust.jcommander.Parameter;
 
 public class Args {
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    @Parameter(description = "Linear System file")
+    private String fileName;
+
     @Parameter(names = {"-d", "--double"}, description = "enable double precision")
-    boolean isDouble = false;
+    private boolean isDouble = false;
 
     @Parameter(names = {"--spp"}, description = "enable scaled partial pivoting")
-    boolean isSPP = false;
+    private boolean isSPP = false;
+
+    public boolean isDouble() {
+        return isDouble;
+    }
+
+    public void setDouble(boolean aDouble) {
+        isDouble = aDouble;
+    }
+
+    public boolean isSPP() {
+        return isSPP;
+    }
+
+    public void setSPP(boolean SPP) {
+        isSPP = SPP;
+    }
 }
