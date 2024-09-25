83d4dd14dd98070a2b99252e9230b37fa6e2a2e8
Sam Ly <sam@samly.me>
2024-09-25 16:11:42 -0700

    implement solve script

---
+++ solve.bash
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+JAR="target/gaussian-1.0-SNAPSHOT-runnable.jar"
+INPUT="data/sys1.lin"
+LOG="data/logs"
+OUTPUT_NAIVE_SINGLE="data/sys1_naive_single.sol"
+OUTPUT_SPP_SINGLE="data/sys1_spp_single.sol"
+OUTPUT_NAIVE_DOUBLE="data/sys1_naive_double.sol"
+OUTPUT_SPP_DOUBLE="data/sys1_spp_double.sol"
+
+if [ ! -f $JAR ]; then
+  echo "Jar not found. Use 'mvn install'."
+  exit 1
+fi
+
+if [ ! -f $INPUT ]; then
+  echo "Input not found."
+  exit 1
+fi
+
+echo "$INPUT solutions"
+
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+
+java -jar $JAR "$INPUT" -o $OUTPUT_NAIVE_SINGLE
+
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+
+java -jar $JAR "$INPUT" --double -o $OUTPUT_NAIVE_DOUBLE
+
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+
+java -jar $JAR "$INPUT" --spp -o $OUTPUT_SPP_SINGLE
+
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+
+java -jar $JAR "$INPUT" --spp --double -o $OUTPUT_SPP_DOUBLE
+
+echo "Successfully solved $INPUT. Solutions stored in  data/."
